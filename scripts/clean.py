839108f9c34ee0bd4a06ca7ad40c48095c4da1cf
Patrick Bradshaw <31942325+PBrad@users.noreply.github.com>
2020-09-24 23:10:15 -0400

    tokenized reviews / best/worst examples

--- scripts/clean.py
+++ scripts/clean.py
@@ -4,6 +4,11 @@
 Created on Thu Sep 24 07:47:27 2020
 
 @author: patrickbradshaw
+
+Clean / filter wine review dataset for Tableau training.
+
+Break off separate dataset of top words used to describe
+wine variety / wines from different countries.
 """
 
 # =============================================================================
@@ -11,6 +16,9 @@ Created on Thu Sep 24 07:47:27 2020
 # =============================================================================
 
 import pandas as pd
+import spacy
+
+nlp = spacy.load('en_core_web_sm', parser = False, entity = False)
 
 # =============================================================================
 # Load
@@ -87,7 +95,75 @@ df_top = df_top.rename(columns = {'index': 'id'})
 
 df_top['description']
 
-# Something like this would work
-#df_new = pd.concat([pd.DataFrame({'description': doc, 'id': row['id']}, index=[0]) 
-#           for _, row in df_top.iterrows() 
-#           for doc in row['docs'].split('.') if doc != ''])
+# Tokenize
+df_top['tokens'] = df_top['description'].str.lower().apply(lambda x: x.split(' '))
+
+# Convert each set of tokens to individual rows (one per token), while retaining index
+df_text = df_top['tokens'].explode()
+
+# Remove punctuation
+df_text = df_text.str.replace('[^\w\s]','')
+
+# Remove stopwords
+spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS       
+
+df_text = df_text[~df_text.isin(spacy_stopwords)] 
+df_text = df_text[~df_text.isin(['wine', 'wines'])]
+
+# Move index to explicit id column
+df_text = df_text.reset_index()
+df_text = df_text.rename(columns = {'index': 'id'})
+
+# Rejoin info about each wine from df_top
+df_top.columns
+del df_top['tokens']
+
+df_text = pd.merge(df_text, df_top, how = 'left', on = 'id')
+
+# Get top 50 words for each variety and country
+df_top_words = pd.DataFrame()
+
+for var in ['variety', 'country']:
+    print(var) 
+    tmp_df = df_text.groupby(var)['tokens'].value_counts(sort = True)
+    tmp_df = tmp_df.groupby(var).head(50) 
+    tmp_df = pd.DataFrame(tmp_df)
+    tmp_df = tmp_df.rename(columns = {'tokens': 'count'}).reset_index()
+    tmp_df = tmp_df.rename(columns = {'variety': 'variable', 'country': 'variable'})
+    tmp_df['tag'] = var
+    
+    df_top_words = df_top_words.append(tmp_df, ignore_index = True)
+
+df_top_words.groupby(['tag'])['variable'].value_counts()
+
+# TODO
+# Could also look at top words for most expensive / least expensive; 
+# and best rated / worst rated
+
+# =============================================================================
+# Best/Worst Review Examples
+# =============================================================================
+
+df_best = df_top.sort_values(by = 'points', ascending = False).groupby(['variety']).head(5)
+df_best = df_best[['description', 'points', 'price', 'variety', 'country']]
+df_best['tag'] = 'best reviews'
+
+df_worst = df_top.sort_values(by = 'points', ascending = False).groupby(['variety']).tail(5)
+df_worst = df_worst[['description', 'points', 'price', 'variety', 'country']]
+df_worst['tag'] = 'worst reviews'
+
+df_best_worst = df_best.append(df_worst, ignore_index = True)
+
+# =============================================================================
+# Write out
+# =============================================================================
+
+# Main review dataset
+del df_top['description'] # drop full text descriptions
+df_top.to_csv('res/wine_main.csv')
+
+# Top words
+df_top_words.to_csv('res/wine_top_words.csv')
+
+# Best/Worst Review Examples
+df_best_worst.to_csv('res/wine_best_worst.csv')
